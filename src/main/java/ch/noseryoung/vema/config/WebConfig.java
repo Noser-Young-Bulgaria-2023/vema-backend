b7d9051702043f68dd540eec634ff46e2b5886ea
Mateusz <mateusz.dettlaff0@gmail.com>
2023-05-10 17:55:51 +0300

    VEMA-14 Added Cors exception

---
+++ src/main/java/ch/noseryoung/vema/config/WebConfig.java
@@ -0,0 +1,27 @@
+package ch.noseryoung.vema.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class WebConfig {
+
+    @Value("${vema.application.frontend.origin}")
+    private String frontendOrigin;
+
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedOrigins(frontendOrigin);
+            }
+        };
+    }
+}
